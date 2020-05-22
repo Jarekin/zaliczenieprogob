6d5e65adffa022846280a416a5dede415750700a
jarekin <65778434+Jarekin@users.noreply.github.com>
2020-05-22 18:34:55 +0200

    Add files via upload

---
+++ FinalTest/Parking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalTest
+{
+    class Parking
+    {
+        public Parking()
+        {
+
+        }
+        public Parking(int miejsca, bool czyPietrowy, int metrazX, int metrazY, List<Auta> autaParking, string kierowca)
+        {
+            Miejsca = miejsca;
+            CzyPietrowy = czyPietrowy;
+            MetrazX = metrazX;
+            MetrazY = metrazY;
+            Auta = autaParking;
+            Kierowca = kierowca;
+        }
+        public int Miejsca
+        { get; set; }
+        public bool CzyPietrowy
+        { get; set; }
+        public int MetrazX
+        { get; set; }
+        public int MetrazY
+        { get; set; }
+        public List<Auta> Auta { get; set; }
+        public string Kierowca { get; set; } 
+
+    }
+
+}
